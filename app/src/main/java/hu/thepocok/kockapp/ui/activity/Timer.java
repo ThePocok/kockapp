1609a0db243015f3964ff415784f8e85a7d9016d
ThePocok <arany.peter@hotmail.com>
2022-05-22 08:29:16 +0200

    Added Timer activity, digital clock font

---
+++ app/src/main/java/hu/thepocok/kockapp/ui/activity/Timer.java
@@ -0,0 +1,16 @@
+package hu.thepocok.kockapp.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hu.thepocok.kockapp.R;
+
+public class Timer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+}
