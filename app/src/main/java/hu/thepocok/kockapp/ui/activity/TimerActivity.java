7e5c61fdc93fab8cd22c1348bc954708cf50afd8
ThePocok <arany.peter@hotmail.com>
2022-05-23 17:50:53 +0200

    #25 - Timer now shows te exact time result when stopped

--- app/src/main/java/hu/thepocok/kockapp/ui/activity/TimerActivity.java
+++ app/src/main/java/hu/thepocok/kockapp/ui/activity/TimerActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -29,6 +30,7 @@ public class TimerActivity extends AppCompatActivity {
     private final String apiURL = "http://thepocok.freeddns.org:3092/records";
     private Button leftBtn;
     private Button rightBtn;
+    TextView timer;
 
     private boolean isLeftTouched = false;
     private boolean isRightTouched = false;
@@ -50,7 +52,8 @@ public class TimerActivity extends AppCompatActivity {
         queue = Volley.newRequestQueue(this);
 
         stopwatch = new Stopwatch();
-        stopwatch.setTextView(findViewById(R.id.elapsed_time_view));
+        timer = findViewById(R.id.elapsed_time_view);
+        stopwatch.setTextView(timer);
 
         leftBtn = findViewById(R.id.button_left);
         rightBtn = findViewById(R.id.button_right);
@@ -139,10 +142,35 @@ public class TimerActivity extends AppCompatActivity {
             stopwatch.stop();
         }
 
+        timer.setText(formatElapsedTime(stopwatch.getElapsedTime()));
+
         // Necessary to make sure, that the timer will not restart when releasing the buttons
         isLeftTouched = false;
         isRightTouched = false;
         isTimerStarted = false;
+    }
+
+    private String formatElapsedTime(long elapsedTime){
+        int minutes = (int) (elapsedTime / (60 * 1000) % 60);
+        int seconds = (int) ((elapsedTime / 1000) % 60);
+        int milliseconds = (int) ((elapsedTime % 1000) / 10);
+
+        StringBuilder sb = new StringBuilder();
+
+        if (minutes != 0) {
+            sb.append((minutes < 10) ? "0" : "");
+            sb.append(minutes);
+            sb.append(":");
+        }
+
+        sb.append((seconds < 10) ? "0" : "");
+        sb.append(seconds);
+        sb.append(".");
+
+        sb.append((milliseconds < 10) ? "0" : "");
+        sb.append(milliseconds);
+
+        return sb.toString();
     }
 
     public void saveResultToRemoteDatabase(int cubeSize, long result) {
